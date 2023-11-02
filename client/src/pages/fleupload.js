d836ff4fcdba4814742785b202a32007e9c5b883
Aarush Aggarwal <76877384+Aarushaggarwal-theone@users.noreply.github.com>
2023-11-02 21:56:43 +0530

    fileupload basic working

---
+++ client/src/pages/fleupload.js
@@ -0,0 +1,37 @@
+
+import React from 'react';
+import {supabase} from '@/utils/supabaseClient.mjs'
+import {v4 as uuidv4} from 'uuid'
+export default function fileUpload() {
+ let [filename, setfilename] = React.useState('');
+  let [file, setfile] = React.useState([]);
+  
+  let [filetype, setfiletype] = React.useState(".pdf");
+  async function submit() {
+    
+
+    let { data : {session} } = await supabase.auth.getSession()
+    console.log(session.user.id)
+    let myuuid = uuidv4();
+    const file_name = session.user.id.concat('/').concat(filename).concat('.pdf')
+    const { data, error } = await supabase
+    .storage
+    .from('forumNotes')
+    .upload(file_name, file, {
+        cacheControl: '3600',
+        upsert: false
+    })
+
+    if (!error) {
+        window.location.href = "/"
+    }
+  }
+  
+  return(
+    <div>
+  <input type="file" id="myFile" name="filename" onChange={(e)=> {setfile([e.target.files[0]])}}/>
+    <input type="text" className="w-fit h-fit p-5" placeholder="thisvalueisshit" onChange={(e) => {setfilename(e.target.value)}}/>
+  <button className="w-fit h-fit p-5" onClick={(e) => {submit()}}>submit</button>
+</div>
+  )
+}
--- client/src/utils/supabaseClient.mjs
+++ client/src/utils/supabaseClient.mjs
@@ -1,4 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
-import { NEXT_PUBLIC_SUPABASE_ANON_KEY, NEXT_PUBLIC_SUPABASE_URL } from './masterfulenv'
+import { NEXT_PUBLIC_SUPABASE_ANON_KEY, NEXT_PUBLIC_SUPABASE_URL } from './masterfulenv.mjs'
 const supabase = createClient(NEXT_PUBLIC_SUPABASE_URL, NEXT_PUBLIC_SUPABASE_ANON_KEY)
 export {supabase}
