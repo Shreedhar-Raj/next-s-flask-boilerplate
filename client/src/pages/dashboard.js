9fd639954e67a76d319c871a1f3d362f4434684e
Shreedhar Raj <shreedharraj.buisness@gmail.com>
2023-11-02 20:19:36 +0530

    nice

--- client/src/pages/dashboard.js
+++ client/src/pages/dashboard.js
@@ -1,5 +1,6 @@
+import Sidebar from '@/components/Sidebar';
 import React from 'react'
-import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+
 import { useState } from 'react';
 
 
@@ -16,51 +17,7 @@ const [open, setOpen] = useState(true);
     { title: "Setting", src: "Setting" },
   ];
   return (
-    <div className="flex">
-      <div
-        className={` ${
-          open ? "w-72" : "w-20 "
-        } bg-black h-screen p-5 bg pt-8 relative duration-300`}
-      >
-        <img
-          src="./src/assets/control.png"
-          className={`absolute cursor-pointer -right-3 top-9 w-7 border-dark-purple
-           border-2 rounded-full  ${!open && "rotate-180"}`}
-          onClick={() => setOpen(!open)}
-        />
-        <div className="flex gap-x-4 items-center">
-          <img
-            src="./src/assets/logo.png"
-            className={`cursor-pointer duration-500 ${
-              open && "rotate-[360deg]"
-            }`}
-          />
-          <h1
-            className={`text-white origin-left font-medium text-xl duration-200 ${
-              !open && "scale-0"
-            }`}
-          >
-            ilovebenstokes
-          </h1>
-        </div>
-        <ul className="pt-6">
-          {Menus.map((Menu, index) => (
-            <li
-              key={index}
-              className={`flex  rounded-md p-2 cursor-pointer hover:bg-light-white text-gray-300 text-sm items-center gap-x-4 
-              ${Menu.gap ? "mt-9" : "mt-2"} ${
-                index === 0 && "bg-light-white"
-              } `}
-            >
-              <img src={`./src/assets/${Menu.src}.png`} />
-              <span className={`${!open && "hidden"} origin-left duration-200`}>
-                {Menu.title}
-              </span>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
+    <Sidebar />
   
   )
 }
