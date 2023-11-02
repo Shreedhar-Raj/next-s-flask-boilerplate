e7755768bfed920b227b6aea100e87f3696644f0
Shreedhar Raj <shreedharraj.buisness@gmail.com>
2023-11-02 21:38:44 +0530

    file upload frontend done

--- client/src/pages/ai-learning/upload.js
+++ client/src/pages/ai-learning/upload.js
@@ -1,8 +1,23 @@
+import Sidebar from '@/components/Sidebar'
 import React from 'react'
+import { useState } from 'react'
+import UploadFileIcon from '@mui/icons-material/UploadFile';
 
 function Upload() {
   return (
-    <div>Upload</div>
+    <div className='flex'>
+      <div>
+        <Sidebar />
+      </div>
+      <div className='flex justify-center items-center h-screen w-screen'>
+      <div className='flex'>
+        <button className='bg-green-300 items-center rounded-xl p-6 border border-2 border-green-400'>Upload File  
+         <UploadFileIcon className='pl-1' />
+        </button>
+      </div>
+
+      </div>
+    </div>
   )
 }
 
