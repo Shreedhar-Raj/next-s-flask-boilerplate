491e9648e49ab3fb12071dec634148a142ed0237
Shreedhar Raj <shreedharraj.buisness@gmail.com>
2023-11-04 16:00:04 +0530

    onboarding done

--- client/src/pages/onboarding/mcq.js
+++ client/src/pages/onboarding/mcq.js
@@ -1,8 +1,34 @@
 import React from 'react'
+import StartIcon from '@mui/icons-material/Start';
 
 function mcq() {
   return (
-    <div>mcq</div>
+    <div>
+        <div className='flex items-center justify-center w-screen h-screen'>
+            <div>
+            <h1 className='text-4xl pb-5'>where is da chaddha?</h1>
+            <div className='flex pt-3 items-center'>
+                <button className='py-1 px-3 rounded-xl border border-orange-500 text-orange-400 hover:text-white hover:bg-orange-400'>A</button>
+                <p className='pl-3'>PARIS</p>
+            </div>
+            <div className='flex pt-3 items-center'>
+                <button className='py-1 px-3 rounded-xl border border-orange-500 text-orange-400 hover:text-white hover:bg-orange-400'>B</button>
+                <p className='pl-3'>PARIS</p>
+            </div>
+            <div className='flex pt-3 items-center'>
+                <button className='py-1 px-3 rounded-xl border border-orange-500 text-orange-400 hover:text-white hover:bg-orange-400'>C</button>
+                <p className='pl-3'>PARIS</p>
+            </div>
+            <div className='flex pt-3 items-center'>
+                <button className='py-1 px-3 rounded-xl border border-orange-500 text-orange-400 hover:text-white hover:bg-orange-400'>D</button>
+                <p className='pl-3'>PARIS</p>
+            </div>
+            <div className='flex pt-3 items-center'>
+                <button className='py-1 items-center px-3 rounded-xl border border-orange-500 text-orange-400 hover:text-white hover:bg-orange-400'>Next Question <StartIcon /></button>
+            </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
--- client/src/pages/onboarding/time.js
+++ client/src/pages/onboarding/time.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 function time() {
   return (
-    <div>time</div>
+    <div>
+        <div className='flex items-center justify-center w-screen h-screen'>
+            <div>
+            <h1 className='text-2xl'>How much time are you willing to spend every day? eg-2h</h1>
+            <input className='w-full border-b-2 outline-0 border-orange-400 text-2xl text-center pt-10' placeholder='chaddha in paris' />
+            </div>
+        </div>
+    </div>
   )
 }
 
