4f2abe250824039e27edb058f74c8f7cb67015be
Shreedhar Raj <shreedharraj.buisness@gmail.com>
2023-11-05 19:43:52 +0530

    love all f yuo

--- client/src/components/PageCard.js
+++ client/src/components/PageCard.js
@@ -1,20 +1,21 @@
-import React from 'react'
+import React from 'react';
 
 const PageCard = (props) => {
   return (
-      <div className="max-w-sm rounded overflow-hidden border border-1 shadow-xl">
+    <div className="max-w-md w-fit rounded-xl border border-1 shadow-xl">
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2">{props.data.title}</div>
-        <p className="text-gray-700 text-base">{props.data.description}</p>
+        <p className="text-gray-700 w-parent text-base whitespace-normal">
+          {props.data.description}
+        </p>
       </div>
       <div className="px-6 py-4">
-        <button
-          className="bg-orange-400 hover:bg-orange-500 text-white font-bold py-2 px-4 rounded">
+        <button className="bg-orange-400 hover:bg-orange-500 text-white font-bold py-2 px-4 rounded">
           {props.data.action}
         </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default PageCard
+export default PageCard;
--- client/src/pages/learning-paths/index.js
+++ client/src/pages/learning-paths/index.js
@@ -9,27 +9,28 @@ function index() {
   const page={
     page: "Learning Paths"
   }
-  const courses = [
+  const geography = 
     {
       title: "Something",
-    description: "nice",
+    description: "niceadsfgeriugbe4ruighuirgbu4gbui24gbui4gbiu4gbi4gbfergergvge",
     action: "Take Course"
-    },
+    }
+    const history = 
     {
       title: "Something",
     description: "nice",
     action: "Take Course"
     }
-  ]
   return (
-    <div className='flex'>
+    <div className='flex max-w-screen'>
       <div>
         <Sidebar />
       </div>
       <div className=' w-full relative h-screen'>
         <TopNavbar data={page} />
-        <div className="p-3">
-        <PageCard className="p-3 shadow-2xl" data={courses} />
+        <div className="p-3 flex gap-x-4 gap-y-4">
+        <PageCard className="p-5 shadow-2xl" data={history} />
+        <PageCard className="p-5 shadow-2xl" data={geography} />
         </div>
       </div>
     </div>
