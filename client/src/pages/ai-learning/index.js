8544671de502444f95e0ea024526f51c1cee2540
Shreedhar Raj <shreedharraj.buisness@gmail.com>
2023-11-05 23:56:44 +0530

    done

--- client/src/pages/ai-learning/index.js
+++ client/src/pages/ai-learning/index.js
@@ -5,6 +5,7 @@ import React from 'react'
 import SmartToyIcon from '@mui/icons-material/SmartToy';
 import NoteAltIcon from '@mui/icons-material/NoteAlt';
 import HelpCenterIcon from '@mui/icons-material/HelpCenter';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 function index() {
   const page  = {
@@ -25,6 +26,11 @@ function index() {
     description: "Create interesting and useful questions with AI",
     component: <HelpCenterIcon className='text-5xl text-[#197878]' />
   }
+  const peer = {
+    title: "Find Peers!",
+    description: "Find peers to study with!",
+    component: <AccountCircleIcon className='text-5xl text-[#197878]' />
+  }
   return (
     <div className='flex'>
       <div>
@@ -37,6 +43,7 @@ function index() {
         <LongCard data={ainotes} />
         <LongCard data={summary} />
         <LongCard data={quiz} />
+        <LongCard data={peer} />
         </div>
         </div>
       </div>
